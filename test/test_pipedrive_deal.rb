32335d40bbd5e18b4cd532244c7013e64d684ab3
Barash Asenov <asenov@carlfinance.de>
2020-03-03 13:37:07 +0100

    Initialized new tests.

--- test/test_pipedrive_deal.rb
+++ test/test_pipedrive_deal.rb
@@ -16,7 +16,7 @@ class TestPipedriveDeal < Test::Unit::TestCase
     'value' => '37k'
   }
 
-  should 'execute a valid person request' do
+  should 'execute a valid deal request' do
     stub_request(:post, 'https://api.pipedrive.com/v1/deals?api_token=some-token')
       .with(body: body,
             headers: {
@@ -45,4 +45,32 @@ class TestPipedriveDeal < Test::Unit::TestCase
     assert_equal 72_312, deal.org_id
   end
 
+  should 'execute a invalid deal request' do
+    stub_request(:post, 'https://api.pipedrive.com/v1/deals?api_token=invalid-token')
+      .with(body: body,
+            headers: {
+              'Accept' => 'application/json',
+              'Content-Type' => 'application/x-www-form-urlencoded',
+              'User-Agent' => 'Ruby.Pipedrive.Api'
+            })
+      .to_return(
+        status: 200,
+        body: File.read(File.join(File.dirname(__FILE__), 'data', 'deal_responses/create_deal_body.json')),
+        headers: {
+          'server' => 'nginx/1.2.4',
+          'date' => 'Fri, 01 Mar 2020 14:01:03 GMT',
+          'content-type' => 'application/json',
+          'content-length' => '1260',
+          'connection' => 'keep-alive',
+          'access-control-allow-origin' => '*'
+        }
+      )
+
+    deal = ::Pipedrive::Deal.create(body)
+
+    assert_equal 'Dope Deal', deal.title
+    assert_equal 37, deal.value
+    assert_equal 'EUR', deal.currency
+    assert_equal 72_312, deal.org_id
+  end
 end
